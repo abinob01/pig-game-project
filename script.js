d734b69f6d7117056395a17164600f0472c592de
abinob01 <abinob70@gmail.com>
2025-11-13 04:53:26 -0800

    selecting all html elements using DOM query selector

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-"use strict";
+'use strict';
+
+const btnNew = document.querySelector('.btn-newGame');
+const btnRoll = document.querySelector('.btn-rollDice');
+const btnHold = document.querySelector('.btn-hold');
+const currentScore = document.querySelector('.currentScore');
+const totalScore = document.querySelector('.totalScore');
+const currentScore1 = document.querySelector('.score-p1');
+const currentScore2 = document.querySelector('.score-p2');
+const totalScore1 = document.querySelector('.totalScore-p1');
+const totalScore2 = document.querySelector('.totalScore-p2');
+const diceIMG = document.querySelector('.img');
+
+// btnRoll = Math.trunc(Math.random() * 6) + 1;
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    diceIMG = Math.trunc(Math.random() * 6) + 1;
+  }
+});
